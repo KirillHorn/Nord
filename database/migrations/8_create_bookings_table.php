8f6d5753ad6ede294b9d0813410318b26fff197e
KirillHorn <122849903+KirillHorn@users.noreply.github.com>
2024-02-06 13:35:34 +0500

    Create DataBase

---
+++ database/migrations/8_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('place_id')->references('id')->on('places');
+            $table->time('beginning_time');
+            $table->time('end_time');
+            $table->foreignId('status_id')->references('id')->on('statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,6 +12,7 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none fs-2">
+          <img alt="" src="/img/Nord1.png" style="width:200px; height:100px;"> 
         </a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" class="nav-link px-2 text-white">Главная</a></li>
@@ -35,3 +36,4 @@
       <hr>
     </div>
 </header>
+
