5e557610275f39b3e78a65ea14d253279a874e2e
KirillHorn <122849903+KirillHorn@users.noreply.github.com>
2024-02-09 11:04:34 +0500

    section index
    dadada

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,20 @@
+
 <x-header/>
-        <section>
+        <section  class="section_back">
             <div class="container">
-                <div style="width: 100%; height:1200px;" > </div>
+                <div style="width: 100%; " class="banner_main d-flex" >
+                    <img>
+                    <div>
+                 <h1>Компьютерный клуб в Уфе</h1>
+
+                 <p>Nord в Уфе - это лучший компьютерный клуб в Уфе, с топовым железом, девайсами и удобным креслами Cougar! Погрузитесь в другую реальность в компьютернрм клубе Nord </p>
+
+                 <a>Забронировать</a>
+                    </div>
+        
                 
             </div>
         </section>
+    </div>
 </body>
 </html>
