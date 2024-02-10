0fced96b2236c5e076d88437f8c38ae12a08790d
KirillHorn <122849903+KirillHorn@users.noreply.github.com>
2024-02-10 21:09:01 +0500

    index almost finished
    section bannera and section information  out

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,32 @@
             <div class="container">
                 <div  class="banner_main d-flex flex-column gap-5 align-self-center" >
 
-                 <h1 class="display-1 fw-3 fw-semibold" >Компьютерный клуб в Уфе</h1>
+              
+                 <h1 class="display-1 fw-3 fw-semibold " >Компьютерный клуб в Уфе</h1>
 
-                 <p>Nord в Уфе - это лучший компьютерный клуб в Уфе, с топовым железом, девайсами и удобным креслами Cougar! Погрузитесь в другую реальность в компьютернрм клубе Nord </p>
-
-                 <a>Забронировать</a>
-
-
-
+                 <p>Nord в Уфе - это лучший компьютерный клуб в Уфе, с топовым железом, девайсами и удобным креслами Cougar! Погрузитесь в другую реальность в компьютерном клубе Nord </p>
+                 <a class="btn btn-warning">Забронировать</a>
             </div>
         </section>
-    </div>
+    <section>
+        <div class="container " >
+            <h2 class="info_sait" >О нас</h2>
+            <div class="d-flex gap-5 info_sait_map">
+                <img alt="местоположение" src="/img/map.jpg" class="map">
+                <div class="d-flex flex-column align-self-center">
+                    <p class="fw-semibold fs-4" > Местоположение: <span style="color: #ffc107 ;" class="fw-bold">Комсомольская, 21/2 ​Советский район</span></p>
+                        <p class="fs-4" style="text-align: justify;">
+                            <span style="color: #ffc107 ;" class="fw-bold"> Nord в Уфе </span>
+                                 - это лучший компьютерный клуб в Уфе, 
+                                 с топовым железом, девайсами и удобным креслами Cougar! 
+                                 Погрузитесь в другую реальность в компьютерном клубе Nord!
+                        </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+
+    </section>
 </body>
 </html>
